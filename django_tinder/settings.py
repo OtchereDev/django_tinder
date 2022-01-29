f12180b44230a1d5ea4ea830f10c5d437ece34cf
Oliver Otchere <Oliverotchere4@gmail.com>
2022-01-29 10:02:50 +0000

    added allauth'

--- django_tinder/settings.py
+++ django_tinder/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # third party apps
+    'allauth',
+    'allauth.account',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +125,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# AllAuth Settings
+
+SITE_ID = 1
